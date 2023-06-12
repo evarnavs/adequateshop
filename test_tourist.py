afad0a8c7394a45909abff89f6e418b751f786f6
evarnavs <107189110+evarnavs@users.noreply.github.com>
2023-06-12 15:55:13 +0400

    update tests (#5)
    * update tests, add yml file
    
    * update tests
    
    * update tests, add rid for creating unique value
    
    * updated and set rid to fixture

--- test_tourist.py
+++ test_tourist.py
@@ -1,6 +1,7 @@
 import requests
 import pytest
 import allure
+from datetime import datetime
 
 base_url = "http://restapi.adequateshop.com"
 
@@ -15,11 +16,12 @@ def login_data():
 
 @pytest.fixture
 def create_tourist():
+    rid = datetime.now().strftime("%Y%m%d%H%M%S")  # random id
     create_url = base_url + "/api/Tourist"
     create_payload = {
-        "tourist_name": "string",
-        "tourist_email": "string123",
-        "tourist_location": "string",
+        "tourist_name": f"user{rid}",
+        "tourist_email": f"email{rid}@email.com",
+        "tourist_location": "neverland",
         "createdat": "2023-06-11T15:59:58.024Z"
     }
     response = requests.post(create_url, json=create_payload)
@@ -42,22 +44,29 @@ def test_login_successful(login_data):
 def test_login_incorrect_credentials():
     url = base_url + "/api/AuthAccount/Login"
     payload = {
-        "username": "incorrect_username",
+        "email": "incorrect_username",
         "password": "incorrect_password"
     }
     response = requests.post(url, json=payload)
-    assert response.status_code == 401
+    assert response.status_code == 200
+    assert response.json()["code"] == 1
+    assert response.json()["message"] == "invalid username or password"
+    assert response.json()["data"] is None
 
 
 @allure.title("Login - Empty Credentials")
 def test_login_empty_credentials():
     url = base_url + "/api/AuthAccount/Login"
     payload = {
-        "username": "",
+        "email": "",
         "password": ""
     }
     response = requests.post(url, json=payload)
     assert response.status_code == 400
+    response_json = response.json()
+    assert response_json["Message"] == "The request is invalid."
+    assert response_json["ModelState"]["log.email"] == ["field is required"]
+    assert response_json["ModelState"]["log.password"] == ["field is required"]
 
 
 @allure.title("Delete Tourist - Successful")
@@ -77,7 +86,7 @@ def test_delete_tourist_twice(create_tourist):
     assert response.status_code == 200
     # Try to delete the same tourist again
     response = requests.delete(delete_url)
-    assert response.status_code == 404  # Assuming 404 is returned for non-existing tourist
+    assert response.status_code == 404  # for non-existing tourist
 
 
 @allure.title("Delete Tourist - Non-existing Tourist")
@@ -86,4 +95,3 @@ def test_delete_non_existing_tourist():
     delete_url = base_url + f"/api/Tourist/{tourist_id}"
     response = requests.delete(delete_url)
     assert response.status_code == 404
-
